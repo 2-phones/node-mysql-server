841c79dab41f67c54d12e7fc5716ef997348f4d8
JoEunsoo <dcpg95@gmail.com>
2022-09-19 14:44:10 +0900

    refactor : 회원 조회  코드 변경

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/login',(req,res) => {
 app.post('/signup', (req,res) => {
     console.log(req.body);
     const { id,email,pw } = req.body;
-    const useradd = `Insert into userlist (Name,Email,Password) values(${id},${email},${pw});`;
+    const useradd = `Insert into userlist (Name,Email,Password) values('${id}','${email}',${pw});`;
     connection.query(useradd,(error,rows) =>{
         error ? res.status(404).send('중복된 아이디 입니다!')
         : res.status(200).send('가입완료!');
