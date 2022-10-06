696b5aa797d2cbf40feb3e5602d9553d77d20e12
JoEunsoo <dcpg95@gmail.com>
2022-10-07 00:01:33 +0900

    feat : 채팅 서버구현

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 import express from "express";
 import cors from "cors";
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import http from "http";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const socketSever = http.createServer(app).listen(PORT,  () => {
-    console.log(`서버 정상적으로 열렷음 `);
+const socketSever = http.createServer(app).listen(PORT, () => {
+  console.log(`서버 정상적으로 열렷음 `);
 });
 
-const socket = new Server( socketSever ,{
-    cors : {
-        origin : '*'
-    }
+const socket = new Server(socketSever, {
+  cors: {
+    origin: "*",
+  },
 });
 
 app.use(cors());
 
-app.get('/' , (req,res) => {
-    res.send('서버 열렸음요!!!');
+app.get("/", (req, res) => {
+  res.send("서버 열렸음요!!!");
 });
 
-socket.on('connect' , msg => {
-    msg.on("message", test => {
-        console.log(`메세지요청 왔음! : ${test.user}`);
-        socket.emit("message" , test.user);
-    })
+socket.on("connect", (msg) => {
+  msg.on("message", (test) => {
+    console.log(`메세지요청 왔음! : ${test}`);
+    socket.emit("message", test);
+  });
 });
-
-
